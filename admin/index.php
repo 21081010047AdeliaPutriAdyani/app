4f051411ea54493f8fce769529ea167776a098a3
21081010047AdeliaPutriAdyani <21081010047@student.upnjatim.ac.id>
2023-06-09 19:51:19 +0700

    index

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,8 @@
 
-<?php include('partials/menu.php'); ?>
+<?php include('menu.php'); ?>
 
-        <!-- Main Content Section Starts -->
         <div class="main-content">
-            <div class="wrapper">
+            <div class="container">
                 <h1>Dashboard</h1>
                 <br><br>
                 <?php 
@@ -18,76 +17,57 @@
                 <div class="col-4 text-center">
 
                     <?php 
-                        //Sql Query 
                         $sql = "SELECT * FROM tbl_category";
-                        //Execute Query
                         $res = mysqli_query($conn, $sql);
-                        //Count Rows
                         $count = mysqli_num_rows($res);
                     ?>
 
                     <h1><?php echo $count; ?></h1>
                     <br />
-                    Categories
+                    Kategori
                 </div>
 
                 <div class="col-4 text-center">
 
                     <?php 
-                        //Sql Query 
                         $sql2 = "SELECT * FROM tbl_food";
-                        //Execute Query
                         $res2 = mysqli_query($conn, $sql2);
-                        //Count Rows
                         $count2 = mysqli_num_rows($res2);
                     ?>
 
                     <h1><?php echo $count2; ?></h1>
                     <br />
-                    Foods
+                    Makanan
                 </div>
 
                 <div class="col-4 text-center">
                     
                     <?php 
-                        //Sql Query 
                         $sql3 = "SELECT * FROM tbl_order";
-                        //Execute Query
                         $res3 = mysqli_query($conn, $sql3);
-                        //Count Rows
                         $count3 = mysqli_num_rows($res3);
                     ?>
 
                     <h1><?php echo $count3; ?></h1>
                     <br />
-                    Total Orders
+                    Total Order
                 </div>
 
                 <div class="col-4 text-center">
                     
                     <?php 
-                        //Creat SQL Query to Get Total Revenue Generated
-                        //Aggregate Function in SQL
                         $sql4 = "SELECT SUM(total) AS Total FROM tbl_order WHERE status='Delivered'";
-
-                        //Execute the Query
                         $res4 = mysqli_query($conn, $sql4);
-
-                        //Get the VAlue
                         $row4 = mysqli_fetch_assoc($res4);
-                        
-                        //GEt the Total REvenue
                         $total_revenue = $row4['Total'];
-
                     ?>
 
                     <h1>$<?php echo $total_revenue; ?></h1>
                     <br />
-                    Revenue Generated
+                    Total Harga
                 </div>
 
                 <div class="clearfix"></div>
 
             </div>
         </div>
-        <!-- Main Content Setion Ends ?php include('partials/footer.php') ?>
